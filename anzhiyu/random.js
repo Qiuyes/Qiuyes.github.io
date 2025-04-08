4f2311d950e289e64054a76d1da58df7beabfd4a
Summer <993879071@qq.com>
2025-04-08 09:37:26 +0800

    Site updated: 2025-04-08 09:37:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/07/hello-world/","2025/04/07/test01/"];function toRandomPost(){
+var posts=["2025/04/07/hello-world/","2025/04/07/test01/","2025/04/08/test01 - 副本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
